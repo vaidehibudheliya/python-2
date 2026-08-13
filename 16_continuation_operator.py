09b6c63ca209a6eef951de4dd9bffb9142a75a94
vaidehibudheliya <vaidehibudheliya@gmail.com>
2026-08-13 19:55:29 +0530

    Add files via upload

---
+++ 16_continuation_operator.py
@@ -0,0 +1,16 @@
+maths = int(input("Enter maths marks:"))
+english = int(input("Enter english marks:"))
+science = int(input("Enter science marks:"))
+computer = int(input("Enter computer marks:"))
+physics = int(input("Enter physics marks:"))
+chemistry = int(input("Enter chemistry marks:"))
+biology = int(input("Enter biology marks:"))
+geography = int(input("Enter geography marks:"))
+drawing = int(input("Enter drawing marks:"))
+
+total = \
+maths + english + science + \
+computer + physics + chemistry + \
+biology + geography + drawing 
+
+print("total = ",total)
